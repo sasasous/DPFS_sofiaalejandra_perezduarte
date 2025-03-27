ce0942f6bf7f36893fe24ea7d1c8c53bed1efe07
SOuS <t4perezduartesofia@gmail.com>
2025-03-27 15:33:55 -0300

    app

---
+++ SPRINT 3/app.js
@@ -0,0 +1,12 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var productosRouter = require('./routes/productos');
+
+var app = express();
+
