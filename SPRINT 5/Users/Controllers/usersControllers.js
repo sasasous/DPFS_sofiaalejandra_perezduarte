44234c3bf9f79eded395541fae8f262c83e59ea9
SOuS <t4perezduartesofia@gmail.com>
2025-04-01 01:53:18 -0300

    usercontroller

---
+++ SPRINT 5/Users/Controllers/usersControllers.js
@@ -0,0 +1,25 @@
+const { title } = require("process");
+
+let usersController = {
+    index: function (req, res) {
+    },
+    show: function (req, res) {
+        return res.send("User: $(req.params.id)");
+    },
+    create: function (req, res) {
+        return res.send("Create User", {title: "Create User"});
+    },
+    search: function (req, res) {
+        let searchTerm = req.query.search
+        return res.render("searchResults", {
+            title: "Search Results",
+            searchTerm: searchTerm
+        });
+    },
+    store: function (req, res) {
+        let info = req.body;
+        req.session.user = info;
+        return res.send(req.session);
+        return res.redirect("/");
+    }
+}
--- SPRINT 5/Users/sign_up.js
+++ SPRINT 5/Users/sign_up.js
@@ -1,3 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var productosRouter = require('./routes/productos');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/productos', productosRouter);
+
+// catch 404 and forward to error handler   
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {}; 
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+//Session
+const session = require('express-session')
+app.use(session({
+  secret: 'users',
+  resave: false,
+  saveUninitialized: true,
+}))
+
 const express = require('express')
 const multer  = require('multer')
 const upload = multer({ dest: 'uploads/' })
@@ -40,3 +88,5 @@ const hash = bcrypt.hashSync("bacon", 10);
 const fs = require('fs');
 const path = require('path');
 const filePath = path.join(__dirname, 'data.json');
+
+module.exports = app;
