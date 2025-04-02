9ea98d0a8235a0c3ef860fee792baa4504097a85
SOuS <t4perezduartesofia@gmail.com>
2025-04-02 10:36:41 -0300

    1

--- SPRINT 6/indexController.js
+++
@@ -1,6 +0,0 @@
-const userController = require("./userController"); 
-var controladores = {}; 
-controladores.usuario = userController; 
-app.use("/usuario", users);
-
-módulo.exports = controladores;
