489e0843cd35b170c8fdac00096ca397580bd30e
SOuS <t4perezduartesofia@gmail.com>
2025-04-03 09:27:41 -0300

    usuario

--- SPRINT 3/database/models/Usuario.js
+++ SPRINT 3/database/models/Usuario.js
@@ -9,20 +9,36 @@ let cols = {
     },
     nombre: {
         type: DataTypes.STRING(100),
+        allowNull: false
     },
-    correo: {
-        type: DataTypes.STRING(150),
-    },
-    password: {
+    apellido: {
         type: DataTypes.STRING(100),
         allowNull: false
     },
-    avatar:{
+    correo: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        unique: true
+    },
+    contrasena: {
         type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    categoria :{
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    imagen :{
+        type: DataTypes.STRING(255),
         allowNull:true
     }
 }
 
+let config = {
+    tableName: 'Usuario',
+    timestamps: false,
+    underscored: true
+}
 
 const Usuario = sequelize.define(alias, cols,config)
 return Usuario;
