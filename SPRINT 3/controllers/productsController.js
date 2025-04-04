4099aea816142fcf77521fa4839fd147cf8bedb2
SOuS <t4perezduartesofia@gmail.com>
2025-04-04 00:09:55 -0300

    productos

--- SPRINT 3/controllers/productsController.js
+++ SPRINT 3/controllers/productsController.js
@@ -1,22 +1,22 @@
-//Controladores de productos
+const { error } = require("console");
+let db = require("../database/models");
+
 let productController = {
-index: function(req, res) {
-    let productList = ["agua", "LEV", "Serpiente", "Love"];
-    return res.render("productos/productos", {
-        title: "Productos",
-        listaIlustraciones: productList
-    });
-},
-show: function(req, res) {
-    return res.sende ("Detalle de ilustracón: $(req.params.id)");
-},
-create: function(req, res) {
-    return res.send("Formulario de creación de ilustración");
-},
-search: function(req, res) {
-    return res.render("searchResults", {
-        title: "Resultados de búsqueda"});
-},
-};
-//Exportamos el controlador
-module.exports = productController;
+    index: function (req, res) {
+        db.Product.findAll()
+            .then(function (data) {
+                return res.send(data);
+                let productList = data;
+                res.render("productos", { title: "Productos", listaProductos: productsList });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },
+    show: function (req, res) {
+        return res.send("Producto: ${req.params.id}");
+    },
+    create: function (req, res) {
+
+    }
+}
