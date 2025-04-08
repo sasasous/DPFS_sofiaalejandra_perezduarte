ec078f86043c99131679741eb2f156a7a8d6ae94
SOuS <t4perezduartesofia@gmail.com>
2025-04-08 14:22:03 -0300

    validator

---
+++ SPRINT 7/middlewares/validator.js
@@ -0,0 +1,27 @@
+const {check} = require("express-validator");
+
+const registroValidator = {
+    nombre: 
+    check("nombre")
+    .notEmpty()
+    .withMessage("El nombre es obligatorio")
+    .bail()
+    .isLength({min: 2})
+    .withMessage("Al menos 2 caracteres"),
+    correo:
+    check("correo")
+    .notEmpty()
+    .withMessage("El correo es obligatorio")
+    .bail()
+    .isEmail()
+    .withMessage("El correo no es válido"),
+    contrasena:
+    check("contrasena")
+    .notEmpty()
+    .withMessage("La contraseña es obligatoria")
+    .bail()
+    .isLength({min: 8})
+    .withMessage("Al menos 8 caracteres")
+};
+
+module.exports = {registroValidator};
