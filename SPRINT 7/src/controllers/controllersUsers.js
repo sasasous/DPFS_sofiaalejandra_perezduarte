0053c1a8ac8ee5956c25e931f107e588db612bb3
SOuS <t4perezduartesofia@gmail.com>
2025-04-15 16:15:24 -0300

    create

--- SPRINT 7/src/controllers/controllersUsers.js
+++ SPRINT 7/src/controllers/controllersUsers.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 module.exports = {
     index: (req, res) => {
-        
+        let users = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/users.jsom')));
+        res.render(path.resolve(__dirname, '../views/users/create'), {users});
+
     }
 }
