954babb15552dcd317dc9a3781463309dd9444a6
SOuS <t4perezduartesofia@gmail.com>
2025-04-20 15:42:21 -0300

    la concha de la loraaaaaaaaaaaaaaaaaa

--- SPRINT 7/src/routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const controllersHome = require
-(path.resolve(__dirname, 'controllers', controllersHome));
-
-router.get('/home', controllersHome.index);
-
-module.exports = router;
