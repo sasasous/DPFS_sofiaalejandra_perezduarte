35676fd83c424f571300e4aa557a97031fe142de
SOuS <t4perezduartesofia@gmail.com>
2025-05-06 14:57:57 -0300

    chachachanges

--- SPRINT 8/sousarte/src/sousarteApp.jsx
+++ SPRINT 8/sousarte/src/sousarteApp.jsx
@@ -1,5 +1,14 @@
+import './sousarteApp.css';
+
 export const sousarteApp = () => {
+
+  //API URLS
+  const urlProducts = './api/products/base.json';
+  const urlUsers = './api/users/base.json';
+
   return (
-    <div>sousarteApp</div>
+    <div className="sousarteApp">
+        <h1> SOUSARTE </h1>
+    </div>
   )
 }
