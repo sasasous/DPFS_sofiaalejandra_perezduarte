8888cc6f54813d75e29ac1d848bafc1ba6783970
SOuS <t4perezduartesofia@gmail.com>
2025-05-07 18:58:31 -0300

    jsx

--- SPRINT 8/sousarte/src/main.jsx
+++ SPRINT 8/sousarte/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { sousarteApp } from './sousarteApp'
+import { sousarteApp } from './sousarteApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- SPRINT 8/sousarte/src/sousarteApp.jsx
+++ SPRINT 8/sousarte/src/sousarteApp.jsx
@@ -6,10 +6,30 @@ export const sousarteApp = () => {
   const urlProducts = './api/products/base.json';
   const urlUsers = './api/users/base.json';
 
+  fetch(urlProducts)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching products:', error);
+    });
+
+  fetch(urlUsers)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching users:', error);
+    });
+
   return (
-    <div className="sousarteApp">
+  <>
+    <div className="Título">
         <h1> SOUSARTE </h1>
     </div>
+  </>
   )
 }
 
